b3e8c0ebec68c35c6c29913bef5cb5441c821c36
NaumenkoEP <egornaumenko2007@gmail.com>
2025-02-27 03:39:42 +0200

    Update handleEmails.js

--- scripts/handleEmails.js
+++ scripts/handleEmails.js
@@ -11,11 +11,22 @@ const sendEmail = () => {
     emailjs.send("service_webVertex", "template_webVertex", user).then(console.log("Success!"))
 }
 const sendForm = () => {
-    sendEmail()
-    setTimeout(function(){
-        document.querySelector(".root").style.opacity = "0"
-        document.querySelector(".success-window").style.display = "flex"
-    }, 1000)
-    formSent = true
-    localStorage.setItem("is form sent", JSON.stringify(formSent))
+    if (
+        document.querySelector(".plan-name").innerText.trim() === "" ||
+        document.getElementById("user-name").value.trim() === "" ||
+        document.getElementById("user-email").value.trim() === "" ||
+        document.getElementById("user-phone").value.trim() === "" ||
+        !document.querySelector('input[name="contact-method"]:checked') || 
+        document.querySelector(".bottom-input").value.trim() === ""
+    ) {
+        alert("Please fill out every input field");
+    } else {
+        sendEmail()
+        setTimeout(function(){
+            document.querySelector(".root").style.opacity = "0"
+            document.querySelector(".success-window").style.display = "flex"
+        }, 1000)
+        formSent = true
+        localStorage.setItem("is form sent", JSON.stringify(formSent))
+    }
 }
